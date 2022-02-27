a2d04d7aff43f0abc6d59b085bfdecdfeee2c2b3
Daviafer <daviafer1@gmail.com>
2022-02-27 20:55:38 -0300

    operadores

---
+++ JavaScript/1-Nível-Básico/3-tipos-primitivos/script.js
@@ -0,0 +1,19 @@
+// String
+let nome = "Nome"
+
+//Number
+let numero = 1
+
+//Boolean
+let usuarioVerificado = false
+if (usuarioVerificado) {
+  console.log("Usuario verificado")
+} else{
+  console.log("Usuário não verificado")
+}
+
+// Null - não vai ser criado
+let pessoa = null
+
+//undefined - indefinido
+let telefone = undefined
---
+++ JavaScript/1-Nível-Básico/4-Operadores-logicos/script.js
@@ -0,0 +1,61 @@
+/* 
+== igual
+!= diferente
+
+=== idêntico
+!== não idêntico
+
+< menor que ( a menor que b)
+> maior que ( a maior que b)
+<= menor igual
+>= maior igual
+
+*/
+
+
+const a = 3
+const b = 3
+
+console.log(a == b)
+console.log(a != b)
+
+console.log(a === b)
+console.log(a !== b)
+
+console.log(a < b)
+console.log(a > b)
+console.log(a <= b)
+console.log(a >= b)
+
+
+// - - - - - - - - - &&  - - - - - - - - 
+
+console.log(a === b && a <= b)
+// V e V = verdadeiro
+
+console.log(a === b && a < b)
+// V e F = False
+
+console.log(a > b && a === b)
+// F e V = False
+
+console.log(a < b && a > b)
+// F e F = False
+
+// - - - - - - - - - - - - - OR  - - - - - - - - - - - - 
+console.log(a === b || a <= b)
+// V ou V = V
+
+console.log(a === b || a < b)
+// V ou F = V
+
+console.log(a > b || a === b)
+// F ou V = V
+
+console.log(a > b || a < b)
+// F ou F = F
+
+// - - - - - - - - - - - - - NOT (negando ) - - - - - - - - - - - - 
+console.log(!(a === b)) // false
+
+console.log(!(a < b)) // true
