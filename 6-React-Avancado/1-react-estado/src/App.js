c6d86544d37dd09161689486b991fc592e779603
Daviafer <daviafer1@gmail.com>
2022-04-19 20:20:37 -0300

    Finalizando aula Ciclo vida na prática - baralho

--- 6-React-Avancado/1-react-estado/src/App.js
+++ 6-React-Avancado/1-react-estado/src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
-import Panel from './components/panel/panel.js'
+// import Panel from './components/panel/panel.js'
+import DeckOfCards from './components/deck-of-cards/deck-of-cards.js';
 
 function App() {
 
   return (
     <>
-    <Panel/>
+    <DeckOfCards/>
     </>
   )
 }
